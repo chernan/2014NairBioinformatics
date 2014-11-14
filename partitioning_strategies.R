531a35314fdc96a1f513aa68200a1d1dd1669df6
Celine Hernandez <celine.hernandez@ens.fr>
2014-11-14 21:05:26 +0100

    Merged basic and shape-based partitioning files.

--- partitioning_strategies.R
+++ partitioning_strategies.R
@@ -1,3 +1,26 @@
+# http://bioinformatics.oxfordjournals.org/content/suppl/2014/05/07/btu318.DC1/nair2014_suppl.pdf
+
+# Expectation-Maximization step for basic ChIP-partitioning algorithm
+#
+# c: a matrix containing the classes to be optimized. c[i,j] is the expected bin count value of class i at position j.
+# q: a vector defining the prior probabilities of each class.
+# data: a matrix containing the samples. data[i,j] is the bin count of sample i at position j.
+em_basic = function(c,q,data) {
+    K=dim(c)[1]; N=dim(data)[1]
+    l=matrix(nrow=N, ncol=K); p=matrix(nrow=N, ncol=K)
+    for(i in 1:N) { for (j in 1:K) {
+        l[i,j] = sum(dpois(data[i,], c[j,], log=T)) }}
+    for(i in 1:N) {
+        p[i,] = q*exp(l[i,]-max(l[i,])); p[i,] = p[i,]/sum(p[i,])}
+    q = colMeans(p)
+    c = (t(p) %*% data)/colSums(p)
+    
+    #c <<-c; q <<-q; p <<-p;
+    return(list(c=c,q=q,p=p))
+}
+
+
+
 # Variations of the EM algorithm - shape-based partitioning
 em_shape = function(c,q,data) {
     K=dim(c)[1]; N=dim(data)[1]
@@ -10,7 +33,9 @@ em_shape = function(c,q,data) {
         p[i,] = q*exp(l[i,]-max(l[i,])); p[i,] = p[i,]/sum(p[i,])}
     q = colMeans(p)
     c = (t(p) %*% data)/colSums(p)
-    c <<-c; q <<-q; p <<-p;
+    
+    #c <<-c; q <<-q; p <<-p;
+    return(list(c=c,q=q,p=p))
 }
 
 # Shape-based partitioning with flips
@@ -28,11 +53,13 @@ em_shape_flip = function(c,q,data) {
     q = apply(p, c(2,3), mean)
     c = (t(p[,,1]) %*% data) + (t(p[,,2]) %*% t(apply(data,1,rev)))
     c = c / apply(p,2,sum)
-    c <<-c; q <<-q; p <<-p;
+    
+    #c <<-c; q <<-q; p <<-p;
+    return(list(c=c,q=q,p=p))
 }
 
 # Shape-based partitioning with shifts
-em_shape_shift = function(c,q,data) {
+em_shape_shift <- function(c,q,data) {
     K=dim(c)[1];
     L=dim(c)[2];
     N=dim(data)[1];
@@ -54,7 +81,9 @@ em_shape_shift = function(c,q,data) {
         q[i,] = sum(q[i,]) * dnorm(1:S,floor(S/2)+1,s) /
             sum(dnorm(1:S,floor(S/2)+1,s))
     }
-    c <<-c; q <<-q; p <<-p;
+    
+    #c <<-c; q <<-q; p <<-p;
+    return(list(c=c,q=q,p=p))
 }
 
 # Shape-based partitioning with flips and shifts
@@ -87,5 +116,7 @@ em_shape_shift_flip = function(c,q,data) {
         q[i,,1] = q[i,,2] = sum(q[i,,]) * dnorm(1:S,floor(S/2)+1,s) /
             sum(dnorm(1:S,floor(S/2)+1,s)) / 2
     }
-    c <<-c; q <<-q; p <<-p;
+    
+    #c <<-c; q <<-q; p <<-p;
+    return(list(c=c,q=q,p=p))
 }
